1b52bf72f3e3f2100739100e04ea65c10daf495b
UnkAtreus <unk.atreus@gmail.com>
2023-05-09 22:01:22 +0700

    :chart_with_upwards_trend: (add defer)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
                 <link rel="icon" href="/logo.ico" />
-                <Script src={'/js/prod_hotjar.js'}></Script>
+                <script src={'/js/prod_hotjar.js'} defer />
             </Head>
             <body>
                 <title>D-Law | DMS-Platform</title>
